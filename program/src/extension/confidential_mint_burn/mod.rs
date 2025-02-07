21ba944a6be545daa3dfa771d62b09a985cf9d8b
samkim-crypto <skim13@cs.stanford.edu>
2025-02-07 22:39:25 +0900

    remove unnecessary constants

--- program/src/extension/confidential_mint_burn/mod.rs
+++ program/src/extension/confidential_mint_burn/mod.rs
@@ -7,14 +7,6 @@ use {
     },
 };
 
-/// Maximum bit length of any mint or burn amount
-///
-/// Any mint or burn amount must be less than `2^48`
-pub const MAXIMUM_DEPOSIT_TRANSFER_AMOUNT: u64 = (u16::MAX as u64) + (1 << 16) * (u32::MAX as u64);
-
-/// Bit length of the low bits of pending balance plaintext
-pub const PENDING_BALANCE_LO_BIT_LENGTH: u32 = 16;
-
 /// Confidential Mint-Burn Extension instructions
 pub mod instruction;
 
