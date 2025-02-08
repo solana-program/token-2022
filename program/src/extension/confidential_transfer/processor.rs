d14c98b27c600a7d917474cbc9a09f70beb291d7
samkim-crypto <skim13@cs.stanford.edu>
2025-02-08 10:39:34 +0900

    [program] Account for the confidential transfer fee extension when re-allocating during configure account (#172)
    * account for confidential transfer fee extension when re-allocating
    
    * add tests for configuring accouts with registry when there are fees
    
    * add check that `TransferFeeAmount` and `ConfidentialTransferFeeAmount` extensions are present
    
    * remove unnecessary logic

--- program/src/extension/confidential_transfer/processor.rs
+++ program/src/extension/confidential_transfer/processor.rs
@@ -157,6 +157,9 @@ fn reallocate_for_configure_account_with_registry<'a>(
     // `try_calculate_account_len` dedupes extension types, so always push
     // the `ConfidentialTransferAccount` type
     current_extension_types.push(ExtensionType::ConfidentialTransferAccount);
+    if current_extension_types.contains(&ExtensionType::TransferFeeAmount) {
+        current_extension_types.push(ExtensionType::ConfidentialTransferFeeAmount);
+    }
     let needed_account_len =
         ExtensionType::try_calculate_account_len::<Account>(&current_extension_types)?;
 
