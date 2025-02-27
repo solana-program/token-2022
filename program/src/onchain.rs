4b8d38ba36bb87f07d96a0580abd67a34e7f436c
Gabe Rodriguez <grod220@gmail.com>
2025-02-27 20:17:48 +0100

    Use SyscallStubs for mocks

--- program/src/onchain.rs
+++ program/src/onchain.rs
@@ -2,22 +2,23 @@
 //! correct accounts
 
 use {
+    crate::state::Mint,
+    solana_program::{
+        account_info::AccountInfo, entrypoint::ProgramResult, program::invoke_signed,
+        pubkey::Pubkey,
+    },
+};
+use {
     crate::{
         extension::{transfer_fee, transfer_hook, StateWithExtensions},
         instruction,
-        state::Mint,
-    },
-    solana_program::{
-        account_info::AccountInfo, entrypoint::ProgramResult, instruction::Instruction,
-        program::invoke_signed, pubkey::Pubkey,
     },
     spl_transfer_hook_interface::onchain::add_extra_accounts_for_execute_cpi,
 };
 
 /// Internal function that uses dependency injection for testing
 #[allow(clippy::too_many_arguments)]
-fn _invoke_transfer_internal<'a, F>(
-    invoker: F,
+fn _invoke_transfer_internal<'a>(
     token_program_id: &Pubkey,
     source_info: AccountInfo<'a>,
     mint_info: AccountInfo<'a>,
@@ -28,10 +29,7 @@ fn _invoke_transfer_internal<'a, F>(
     decimals: u8,
     fee: Option<u64>,
     seeds: &[&[&[u8]]],
-) -> ProgramResult
-where
-    F: Fn(&Instruction, &[AccountInfo<'a>], &[&[&[u8]]]) -> ProgramResult,
-{
+) -> ProgramResult {
     let mut cpi_account_infos = vec![
         source_info.clone(),
         mint_info.clone(),
@@ -90,7 +88,7 @@ where
         }
     }
 
-    invoker(&cpi_instruction, &cpi_account_infos, seeds)
+    invoke_signed(&cpi_instruction, &cpi_account_infos, seeds)
 }
 
 /// Helper to CPI into token-2022 on-chain, looking through the additional
@@ -108,7 +106,6 @@ pub fn invoke_transfer_checked<'a>(
     seeds: &[&[&[u8]]],
 ) -> ProgramResult {
     _invoke_transfer_internal(
-        invoke_signed,
         token_program_id,
         source_info,
         mint_info,
@@ -139,7 +136,6 @@ pub fn invoke_transfer_checked_with_fee<'a>(
     seeds: &[&[&[u8]]],
 ) -> ProgramResult {
     _invoke_transfer_internal(
-        invoke_signed,
         token_program_id,
         source_info,
         mint_info,
@@ -155,46 +151,59 @@ pub fn invoke_transfer_checked_with_fee<'a>(
 
 #[cfg(test)]
 mod tests {
-    use {
-        super::*,
-        solana_program::{
-            account_info::AccountInfo,
-            entrypoint::ProgramResult,
-            instruction::{AccountMeta, Instruction},
-            program_option::COption,
-            program_pack::Pack,
-            pubkey::Pubkey,
-        },
-        std::cell::RefCell,
-    };
+    use solana_program::instruction::AccountMeta;
+    use solana_program::program_option::COption;
+    use solana_program::program_pack::Pack;
+    use std::cell::RefCell;
+    use {super::*, solana_program::instruction::Instruction};
 
-    struct MockInvoker {
-        captured_instruction: Option<Instruction>,
-        captured_account_keys: Option<Vec<Pubkey>>,
-        return_result: ProgramResult,
+    thread_local! {
+        static CAPTURED_INSTRUCTION: RefCell<Option<Instruction>> = RefCell::new(None);
+        static CAPTURED_ACCOUNT_KEYS: RefCell<Option<Vec<Pubkey>>> = RefCell::new(None);
     }
 
-    impl MockInvoker {
-        fn new(return_result: ProgramResult) -> Self {
-            MockInvoker {
-                captured_instruction: None,
-                captured_account_keys: None,
-                return_result,
-            }
-        }
+    fn get_captured_params() -> (Instruction, Vec<Pubkey>) {
+        let instr = CAPTURED_INSTRUCTION.with(|ci| ci.borrow().clone().unwrap());
+        let keys = CAPTURED_ACCOUNT_KEYS.with(|ck| ck.borrow().clone().unwrap());
+        (instr, keys)
+    }
 
-        fn invoke(
-            &mut self,
+    struct SyscallStubs {}
+    impl solana_sdk::program_stubs::SyscallStubs for SyscallStubs {
+        fn sol_invoke_signed(
+            &self,
             instruction: &Instruction,
             account_infos: &[AccountInfo],
+            _signers_seeds: &[&[&[u8]]],
         ) -> ProgramResult {
-            self.captured_instruction = Some(instruction.clone());
-            self.captured_account_keys = Some(account_infos.iter().map(|ai| *ai.key).collect());
-            self.return_result.clone()
+            CAPTURED_INSTRUCTION.with(|ci| {
+                *ci.borrow_mut() = Some(instruction.clone());
+            });
+            let account_keys: Vec<Pubkey> = account_infos.iter().map(|info| *info.key).collect();
+            CAPTURED_ACCOUNT_KEYS.with(|ck| {
+                *ck.borrow_mut() = Some(account_keys);
+            });
+            Ok(())
         }
     }
 
-    // Setup mint data
+    fn reset_globals() {
+        CAPTURED_INSTRUCTION.with(|ci| *ci.borrow_mut() = None);
+        CAPTURED_ACCOUNT_KEYS.with(|ck| *ck.borrow_mut() = None);
+    }
+
+    fn set_stubs() -> ProgramResult {
+        {
+            use std::sync::Once;
+            static ONCE: Once = Once::new();
+
+            ONCE.call_once(|| {
+                solana_sdk::program_stubs::set_syscall_stubs(Box::new(SyscallStubs {}));
+            });
+        }
+        Ok(())
+    }
+
     fn setup_mint() -> Vec<u8> {
         let state = Mint {
             decimals: 10,
@@ -210,6 +219,9 @@ mod tests {
 
     // Parameterized test function
     fn test_invoke_fns_with_params(is_multisig: bool, with_fee: bool) {
+        reset_globals();
+        set_stubs().unwrap();
+
         let token_program_id = crate::id();
         let source_key = Pubkey::new_unique();
         let mint_key = Pubkey::new_unique();
@@ -324,52 +336,56 @@ mod tests {
             vec![signer1_info, signer2_info, signer3_info]
         };
 
-        let mock_invoker = RefCell::new(MockInvoker::new(Ok(())));
-        let invoker_fn =
-            |instruction: &Instruction, account_infos: &[AccountInfo], _seeds: &[&[&[u8]]]| {
-                mock_invoker.borrow_mut().invoke(instruction, account_infos)
-            };
-
-        let fee = if with_fee { Some(120) } else { None };
-
-        let result = _invoke_transfer_internal(
-            invoker_fn,
-            &token_program_id,
-            source_info.clone(),
-            mint_info.clone(),
-            destination_info.clone(),
-            authority_info.clone(),
-            &additional_accounts,
-            200,
-            6,
-            fee,
-            &[],
-        );
-        assert!(result.is_ok());
+        if with_fee {
+            invoke_transfer_checked_with_fee(
+                &token_program_id,
+                source_info.clone(),
+                mint_info.clone(),
+                destination_info.clone(),
+                authority_info.clone(),
+                &additional_accounts,
+                200,
+                6,
+                120,
+                &[],
+            )
+            .unwrap();
+        } else {
+            invoke_transfer_checked(
+                &token_program_id,
+                source_info.clone(),
+                mint_info.clone(),
+                destination_info.clone(),
+                authority_info.clone(),
+                &additional_accounts,
+                200,
+                6,
+                &[],
+            )
+            .unwrap();
+        }
 
         let expected_account_count = if is_multisig { 7 } else { 4 };
 
-        let mock_invoker = mock_invoker.borrow();
-        let instruction = mock_invoker.captured_instruction.as_ref().unwrap();
-        let account_keys = mock_invoker.captured_account_keys.as_ref().unwrap();
+        let (instruction, account_keys) = get_captured_params();
 
         // Check instruction
-        assert_eq!(instruction.program_id, token_program_id,);
-        assert_eq!(instruction.accounts.len(), expected_account_count,);
+        assert_eq!(instruction.program_id, token_program_id);
+        assert_eq!(instruction.accounts.len(), expected_account_count);
 
         // Verify base account metas
-        assert_eq!(instruction.accounts[0], AccountMeta::new(source_key, false),);
+        assert_eq!(instruction.accounts[0], AccountMeta::new(source_key, false));
         assert_eq!(
             instruction.accounts[1],
-            AccountMeta::new_readonly(mint_key, false),
+            AccountMeta::new_readonly(mint_key, false)
         );
         assert_eq!(
             instruction.accounts[2],
-            AccountMeta::new(destination_key, false),
+            AccountMeta::new(destination_key, false)
         );
         assert_eq!(
             instruction.accounts[3],
-            AccountMeta::new_readonly(authority_key, authority_is_signer),
+            AccountMeta::new_readonly(authority_key, authority_is_signer)
         );
 
         // Verify additional signer metas if multisig
