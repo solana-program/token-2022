7aa7a4d26cda4ab449afdc93fc9772c17c1a830f
samkim-crypto <skim13@cs.stanford.edu>
2025-03-28 23:00:17 +0900

    [elgamal-registry] Add security txt for elgamal-registry and add SECURITY.md (#337)
    * add security-txt for elgamal registry
    
    * add SECURITY.md
    
    * Apply suggestions from code review
    
    Co-authored-by: Jon C <me@jonc.dev>
    
    * add boundies and scope to `SECURITY.md`
    
    ---------
    
    Co-authored-by: Jon C <me@jonc.dev>

--- confidential-transfer/elgamal-registry/src/entrypoint.rs
+++ confidential-transfer/elgamal-registry/src/entrypoint.rs
@@ -4,6 +4,7 @@
 
 use {
     solana_account_info::AccountInfo, solana_program_error::ProgramResult, solana_pubkey::Pubkey,
+    solana_security_txt::security_txt,
 };
 
 solana_program_entrypoint::entrypoint!(process_instruction);
@@ -13,4 +14,16 @@ fn process_instruction(
     instruction_data: &[u8],
 ) -> ProgramResult {
     crate::processor::process_instruction(program_id, accounts, instruction_data)
+}
+
+security_txt! {
+    // Required fields
+    name: "SPL ElGamal Registry",
+    project_url: "https://solana-program.com",
+    contacts: "link:https://github.com/solana-program/token-2022/security/advisories/new,mailto:security@anza.xyz,discord:https://solana.com/discord",
+    policy: "https://github.com/solana-program/token-2022/blob/master/SECURITY.md",
+
+    // Optional Fields
+    preferred_languages: "en",
+    source_code: "https://github.com/solana-program/token-2022/tree/master/confidential-transfer/elgamal-registry"
 }
