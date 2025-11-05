b498222bdfc2f7505e0e82ebc3fbc519e38d4be1
Jon C <me@jonc.dev>
2025-11-05 18:54:59 +0100

    Use modern functions

--- clients/cli/tests/config.rs
+++ clients/cli/tests/config.rs
@@ -1,10 +1,15 @@
-use assert_cmd::cmd::Command;
+use assert_cmd::cargo::cargo_bin_cmd;
 
 #[test]
 fn invalid_config_will_cause_commands_to_fail() {
-    let mut cmd = Command::cargo_bin("spl-token").unwrap();
-    cmd.args(["address", "--config", "~/nonexistent/config.yml"]);
-    cmd.assert()
-        .stderr("error: Could not find config file `~/nonexistent/config.yml`\n");
-    cmd.assert().code(1).failure();
+    let output = cargo_bin_cmd!("spl-token")
+        .args(["address", "--config", "~/nonexistent/config.yml"])
+        .output()
+        .unwrap();
+    assert_eq!(
+        std::str::from_utf8(&output.stderr).unwrap(),
+        "error: Could not find config file `~/nonexistent/config.yml`\n"
+    );
+    assert_eq!(output.status.code().unwrap(), 1);
+    assert!(!output.status.success());
 }
